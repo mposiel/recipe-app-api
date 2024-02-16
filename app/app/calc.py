087dbad153aa914cb97c55dd14ace1c59005967b
mposiel <114227935+mposiel@users.noreply.github.com>
2024-02-16 14:28:49 +0100

    Configured db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add two numbers and return the result"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract nums"""
+    return x - y;
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calculator functions in calc.py """
+    def test_add_nums(self):
+        """Test adding nums together"""
+        res = calc.add(3,8)
+        self.assertEqual(res, 11 )
+
+    def test_subtract_nums(self):
+        """Test subtracting nums"""
+        res = calc.subtract(3,8)
+        self.assertEqual(res, -5)
